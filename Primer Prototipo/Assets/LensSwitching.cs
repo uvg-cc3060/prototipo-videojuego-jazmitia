4eedddff0c0df8b563dcf815fe06e42a1e0f2a32
jorge97710 <jazmitia_10@hotmail.com>
2019-05-15 17:18:45 -0600

    init

---
+++ Primer Prototipo/Assets/LensSwitching.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using TMPro;
+public class LensSwitching : MonoBehaviour
+{
+
+    public int selectedLens=0 ;
+    public  TextMeshProUGUI textMesh;
+    string [] lentes = new string[3] { "Thermal", "X-Ray", "Night Vision"};
+    public GameObject key;
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectLens();
+        key = GameObject.FindGameObjectWithTag("key");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+
+        int pevieousSelectedLens = selectedLens;
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+
+            if (selectedLens >= transform.childCount - 1)
+            {
+                selectedLens = 0;
+            }
+            else
+            {
+                selectedLens++;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+
+            if (selectedLens <= 0)
+            {
+                selectedLens = transform.childCount-1;
+            }
+            else
+            {
+                selectedLens--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            selectedLens = 0;
+            key.GetComponent<SpriteRenderer>().sortingLayerName = "Invisible";
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)&& transform.childCount>=2)
+        {
+            selectedLens = 1;
+            key.GetComponent<Renderer>().material.SetColor("_Color", color);
+            key.GetComponent<SpriteRenderer>().sortingLayerName = "Rayos X";
+            key.SetActive(true);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            selectedLens = 2;
+            key.GetComponent<SpriteRenderer>().sortingLayerName = "Invisible";
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        {
+            selectedLens = 3;
+            key.GetComponent<SpriteRenderer>().sortingLayerName = "Invisible";
+
+        }
+
+
+
+        if (pevieousSelectedLens != selectedLens) {
+            SelectLens();
+        }
+        textMesh.text = "Lentes: "+(lentes[selectedLens]).ToString();
+    }
+
+    void SelectLens() {
+        int i = 0;
+        foreach (Transform lens in transform) {
+            if (i == selectedLens)
+            {
+                lens.gameObject.SetActive(true);
+            }
+            else {
+                lens.gameObject.SetActive(false);
+            }
+
+            i++;
+
+        }
+
+    }
+}
---
+++ Primer Prototipo/Assets/Sripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Vector2 moveVelocity;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);      
+    }
+}
